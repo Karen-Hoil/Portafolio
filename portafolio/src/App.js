b83330fd24714bf778bc923c2ad9e308b10516df
Karen-Hoil <karenhoil2004@gmail.com>
2023-09-21 20:50:15 -0500

    Api

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard'; // Importa el componente Dashboard
+import CondicionAdmosferica from './condicionAdmosferica';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Dashboard /> {/* Agrega el componente Dashboard aquí */}
+        <CondicionAdmosferica/>
+
       </header>
     </div>
   );
---
+++ portafolio/src/condicionAdmosferica.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+
+function CondicionAdmosferica() {
+    const url = "https://api.datos.gob.mx/v1/condiciones-atmosfericas";
+    const estadosMx = [{"id":1, "name":"Aguascalientes"},{"id": 2, "name": "Baja California"},{"id": 19, "name": "Nuevo León"},{"id": 20, "name": "Oxaca"},{"id": 21, "name": "Puebla"}, {"id": 31, "name": "Yucatan"}, {"id": 32, "name": "Zacatecas"}];
+    const [datos, setDatos] = useState([]);
+    const [estadoActual, setEstadoActual] = useState("Quintana Roo");
+    const consultarDatos = () => {
+        return fetch(url)
+        .then((res) => res.json())
+        .then((condicionAtm) => setDatos(condicionAtm.results))
+    }
+
+    useEffect(() => {
+        consultarDatos();
+    }, []);
+  return (
+    <>
+        <select on onChange={(e) => setEstadoActual(e.target.value)}>
+            <option value="">Selecciona una opción</option>
+            {estadosMx.map((opcion) => (<option key={opcion.id} value={opcion.name}>
+                {opcion.name}
+            </option>))}
+        </select>
+
+        {estadoActual}
+        <h1>Estado del Tiempo</h1>
+        {datos.map((ciudad, index) =>{
+            return(
+                <div>
+                    <p>{ciudad.name} - <i>{ciudad.skydescriotionlong}</i></p>
+                </div>
+            );
+        })}
+    </>
+  );
+}
+
+export default CondicionAdmosferica
